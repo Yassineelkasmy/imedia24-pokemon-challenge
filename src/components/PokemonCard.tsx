8ed71f9639c2c66ff4018aa37c65c33d7e1f3e6f
Yassine El Kasmy <yassineelkasmy@gmail.com>
2023-06-26 16:47:04 +0100

    add some styles to the pokemon card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,22 @@
 import { IPokemon } from "../models/IPokemon";
+import { motion } from "framer-motion";
 
 interface PokemonCardProps {
     pokemon: IPokemon;
 }
 
 const PokemonCard = ({ pokemon }: PokemonCardProps) => {
+
     return (
-        <div className='flex flex-col justify-center h-48 border-2 border-black rounded-lg text-center text-2xl tracking-wider hover:z-10 hover:-translate-y-3 transition-all hover:shadow-2xl hover:cursor-pointer hover:bg-slate-300' key={pokemon.name}>
+
+        <motion.div whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, animationDuration: "10s" }} className='flex flex-col justify-center h-48 border-2 border-black rounded-lg text-center text-2xl tracking-wider transition-all hover:shadow-2xl hover:cursor-pointer hover:bg-slate-300' key={pokemon.name}>
             <p className="">{pokemon.name.toUpperCase()}</p>
-        </div>
+        </motion.div>
+
+
     )
 
 };
